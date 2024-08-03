b3a4db403c1240b95c7b8ba75aa02c48c8ce5fbb
Luan <131721195+LiuAnderson17@users.noreply.github.com>
2024-08-02 21:42:05 -0300

    Update __init__.py
    Alterando o diretório do banco de dados para local

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db" #os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "5624a7dac9937162a45b3c406f485d2f"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
